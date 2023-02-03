1a7f6ab081b7a4bd1554afa7a2451d955276a0f1
AK DEEPANKAR <62662701+akdeepankar@users.noreply.github.com>
2023-02-03 14:06:43 +0000

    Added Cramers Rule for finding x, y, z in a system of linear Equation of Form Ax = B.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from matrix import Determinant
+from matrix import Cramer
 
-mat = Determinant.order3(3, 1, 1, 4, -2, 5, 2, 8, 7)
+mat = Determinant.order3(1, 0, 1, -1, 1, 1, 0, -1, 1)
 print(mat)
 
-mat2 = Determinant.order2(3, 1, 8, 7)
+mat2 = Determinant.order2(4, -2, -8, 4)
 print(mat2)
 
+mat22 = Cramer.lieq2(4, -3, 6, 5, 11, 7)
+print(mat22)
+
+mat23 = Cramer.lieq3(1, 0, 3, 0, 2, 5, 4, 3, 1, 0, 2, 1)
+print(mat23)
 
--- matrix.py
+++ matrix.py
@@ -10,12 +10,59 @@ class Determinant:
     temp = temp1 - temp2
     return (temp)
 
-  def order3(a1, b1, c1, a2, b2, c2, a3, b3, c3):
-    tempa1 = b2 * c3 - b3 * c2
-    tempb1 = a2 * c3 - a3 * c2
-    tempc1 = a2 * b3 - a3 * b2
-    tempa = a1 * tempa1
-    tempb = b1 * tempb1
-    tempc = c1 * tempc1
+  def order3(a11, a12, a13, a21, a22, a23, a31, a32, a33):
+    tempa1 = a22 * a33 - a23 * a32
+    tempb1 = a21 * a33 - a31 * a23
+    tempc1 = a21 * a32 - a31 * a22
+    tempa = a11 * tempa1
+    tempb = a12 * tempb1
+    tempc = a13 * tempc1
     det = tempa - tempb + tempc
     return (det)
+
+
+class Cramer(Determinant):
+
+  def __init__(self):
+    self.lieq2()
+
+  def lieq2(a11, a12, b21, b22, b1, b2):
+    det = Determinant.order2(a11, a12, b21, b22)
+    print(det)
+
+    if det == 0:
+      print("Determinant of A is 0")
+    else:
+      ax1 = Determinant.order2(b1, a12, b2, b22)
+      print(ax1)
+
+      ax2 = Determinant.order2(a11, b1, b21, b2)
+      print(ax2)
+
+      x1 = int(ax1 / det)
+      x2 = int(ax2 / det)
+      print("x1 = ", x1, "\nx2 = ", x2)
+
+  def lieq3(a11, a12, a13, a21, a22, a23, a31, a32, a33, b1, b2, b3):
+    det = Determinant.order3(a11, a12, a13, a21, a22, a23, a31, a32, a33)
+    print(det)
+
+    if det == 0:
+      print("Determinant of A is 0")
+    else:
+      ax1 = Determinant.order3(b1, a12, a13, b2, a22, a23, b3, a32, a33)
+      print(ax1)
+
+      ax2 = Determinant.order3(a11, b1, a13, a21, b2, a23, a31, b3, a33)
+      print(ax2)
+
+      ax3 = Determinant.order3(a11, a12, b1, a21, a22, b2, a31, a32, b3)
+      print(ax3)
+
+      x1 = ax1 / det
+      x2 = ax2 / det
+      x3 = ax3 / det
+      print("x1 = ", ax1, "/", det, "=", x1)
+      print("x2 = ", ax2, "/", det, "=", x2)
+      print("x3 = ", ax3, "/", det, "=", x3)
+      
