399d816c7f88d850d6b638ca0fd87fa1d87d8f7c
AK DEEPANKAR <62662701+akdeepankar@users.noreply.github.com>
2023-02-03 21:03:39 +0000

    Added Adjugate and A Inverse Classes.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from matrix import Determinant
-from matrix import Cramer
+#from matrix import Cramer
+#from matrix import Adjugate, Ainverse
 
-mat = Determinant.order3(1, 0, 1, -1, 1, 1, 0, -1, 1)
-print(mat)
+mat = Determinant.order3(1,2,3,4,5,6,7,8,9)
+#print(mat)
 
-mat2 = Determinant.order2(4, -2, -8, 4)
-print(mat2)
+#mat2 = Determinant.order2(4, -2, -8, 4)
+#print(mat2)
 
-mat22 = Cramer.lieq2(4, -3, 6, 5, 11, 7)
-print(mat22)
+#mat22 = Cramer.lieq2(4, -3, 6, 5, 11, 7)
+#print(mat22)
 
-mat23 = Cramer.lieq3(1, 0, 3, 0, 2, 5, 4, 3, 1, 0, 2, 1)
-print(mat23)
+#mat23 = Cramer.lieq3(1,2,3,4,5,6,7,8,9,2,3,4)
 
+#adju = Adjugate.adj3(1,2,3,0,2,8,5,6,0)
+#print(adju)
+#ainv = Ainverse.ainv3(1,2,3,4,5,6,7,8,9)
+#adju = Adjugate.adj3(1,2,3,4,5,6,7,8,9)
--- matrix.py
+++ matrix.py
@@ -1,14 +1,20 @@
+#This File was Created by AK DEEPANKAR
+#First Year, BS Data Science, IIT Madras (FEB, 2022)
+
 class Determinant:
 
   def __init__(self):
     self.order2()
     self.order3()
 
-  def order2(a11, a12, b21, b22):
-    temp1 = a11 * b22
-    temp2 = a12 * b21
+  def order2(a11, a12, a21, a22):
+    temp1 = a11 * a22
+    temp2 = a12 * a21
     temp = temp1 - temp2
-    return (temp)
+    if temp == 0:
+     return (temp)
+    else:
+     print("Determinant is 0") 
 
   def order3(a11, a12, a13, a21, a22, a23, a31, a32, a33):
     tempa1 = a22 * a33 - a23 * a32
@@ -18,7 +24,10 @@ class Determinant:
     tempb = a12 * tempb1
     tempc = a13 * tempc1
     det = tempa - tempb + tempc
-    return (det)
+    if det == 0:
+      print("Determinant is 0")
+    else:
+      return (det)
 
 
 class Cramer(Determinant):
@@ -26,17 +35,16 @@ class Cramer(Determinant):
   def __init__(self):
     self.lieq2()
 
-  def lieq2(a11, a12, b21, b22, b1, b2):
-    det = Determinant.order2(a11, a12, b21, b22)
-    print(det)
+  def lieq2(a11, a12, a21, a22, b1, b2):
+    det = Determinant.order2(a11, a12, a21, a22)
 
     if det == 0:
       print("Determinant of A is 0")
     else:
-      ax1 = Determinant.order2(b1, a12, b2, b22)
+      ax1 = Determinant.order2(b1, a12, b2, a22)
       print(ax1)
 
-      ax2 = Determinant.order2(a11, b1, b21, b2)
+      ax2 = Determinant.order2(a11, b1, a21, b2)
       print(ax2)
 
       x1 = int(ax1 / det)
@@ -45,7 +53,6 @@ class Cramer(Determinant):
 
   def lieq3(a11, a12, a13, a21, a22, a23, a31, a32, a33, b1, b2, b3):
     det = Determinant.order3(a11, a12, a13, a21, a22, a23, a31, a32, a33)
-    print(det)
 
     if det == 0:
       print("Determinant of A is 0")
@@ -65,4 +72,65 @@ class Cramer(Determinant):
       print("x1 = ", ax1, "/", det, "=", x1)
       print("x2 = ", ax2, "/", det, "=", x2)
       print("x3 = ", ax3, "/", det, "=", x3)
-      
+
+
+class Adjugate(Determinant):
+
+  def __init__(self):
+    self.adj3()
+    
+
+  def adj3(a11, a12, a13, a21, a22, a23, a31, a32, a33):
+
+    det = Determinant.order3(a11, a12, a13, a21, a22, a23, a31, a32, a33)
+    print("Determinant: ", det)
+
+    m11 = a22 * a33 - a23 * a32
+    m12 = a21 * a33 - a23 * a31
+    m13 = a21 * a32 - a22 * a31
+    m21 = a12 * a33 - a13 * a32
+    m22 = a11 * a33 - a13 * a31
+    m23 = a11 * a32 - a12 * a31
+    m31 = a12 * a23 - a13 * a22
+    m32 = a11 * a23 - a13 * a21
+    m33 = a11 * a22 - a12 * a21
+
+    #cofactor matrix
+    c11 = 1 * m11
+    c12 = -1 * m12
+    c13 = 1 * m13
+    c21 = -1 * m21
+    c22 = 1 * m22
+    c23 = -1 * m23
+    c31 = 1 * m31
+    c32 = -1 * m32
+    c33 = 1 * m33
+    print(c11, c21, c31, c12, c22, c32, c13, c23, c33)
+    return(c11, c21, c31, c12, c22, c32, c13, c23, c33)
+    print("Cofactor Matrix")
+    print(c11, " ", c12, " ", c13)
+    print(c21, " ", c22, " ", c23)
+    print(c31, " ", c32, " ", c33)
+
+    #AdjugateMatrix = transpose of cofactor
+
+    print("Adjugate Matrix")
+    print(c11, " ", c21, " ", c31)
+    print(c12, " ", c22, " ", c32)
+    print(c13, " ", c23, " ", c33)
+
+
+class Ainverse(Adjugate):
+
+  def __init__(self):
+    self.ainv3()
+
+  def ainv3(a11, a12, a13, a21, a22, a23, a31, a32, a33):
+
+    det = Determinant.order3(a11, a12, a13, a21, a22, a23, a31, a32, a33)
+    c = Adjugate.adj3(a11, a12, a13, a21, a22, a23, a31, a32, a33)
+    if det == 0:
+      print("Inverse Does Not Exist")
+    else:
+      result = tuple([i * round(1 / det, 2) for i in c])
+      print("A Inverse: ", result)
